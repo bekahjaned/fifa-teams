826f3b55fb98f3e4f161990d573b20ba6e4008be
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-09-05 20:35:36 -0400

    Add styling when input field is in focus

--- src/Elements/EmailInput/index.js
+++ src/Elements/EmailInput/index.js
@@ -3,8 +3,14 @@ import styled from "styled-components";
 export const EmailInput = styled.input`
   font-size: 16px;
   color: #666e80;
-  padding: 18px;
+  padding-left: 18px;
   border: 1px solid #161921;
   border-radius: 4px;
   grid-column-start: span 4;
+  outline: none;
+  max-height: 56px;
+
+  :focus {
+    border: 2px solid #161921;
+  }
 `;
