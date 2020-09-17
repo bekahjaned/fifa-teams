f33fc6ea56eb336cecbff51a6828b713b43025ab
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-09-17 18:03:48 -0400

    Change Main into a function component that uses useState

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import teams from "../../data/teams.json";
+import theTeams from "../../data/teams.json";
 
 import { MainWrap } from "../../Elements/MainWrap/";
 import { MainContent } from "../../Elements/MainContent/";
@@ -8,44 +8,34 @@ import { CardGrid } from "../../Elements/CardGrid/";
 
 import TeamCard from "../TeamCard/";
 
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
+function Main() {
+  const [teams, setTeams] = useState(theTeams);
 
-    this.state = {
-      teams: teams,
-    };
-  }
-
-  render() {
-    const viewTeams = this.state.teams.map((team) => {
-      const { name, league, image, background, guidelines, objectives } = team;
-      return (
-        <TeamCard
-          name={name}
-          league={league}
-          image={image}
-          background={background}
-          guidelines={guidelines}
-          objectives={objectives}
-          key={name}
-        />
-      );
-    });
-
-    return (
-      <MainWrap>
-        <MainContent>
-          <h1 className="main-title">Team Guidelines</h1>
-          <p className="main-description">
-            If you would like to submit any team ideas, contact u/StillGrowingUp
-            on Reddit.
-          </p>
-          <CardGrid>{viewTeams}</CardGrid>
-        </MainContent>
-      </MainWrap>
-    );
-  }
+  return (
+    <MainWrap>
+      <MainContent>
+        <h1 className="main-title">Team Guidelines</h1>
+        <p className="main-description">
+          If you would like to submit any team ideas, contact u/StillGrowingUp
+          on Reddit.
+        </p>
+        <CardGrid>
+          {teams.map((team, i) => (
+            <TeamCard
+              index={i}
+              name={team.name}
+              league={team.league}
+              image={team.image}
+              background={team.background}
+              guidelines={team.guidelines}
+              objectives={team.objectives}
+              key={team.name}
+            />
+          ))}
+        </CardGrid>
+      </MainContent>
+    </MainWrap>
+  );
 }
 
 export default Main;
--- src/Components/TeamCard/index.js
+++ src/Components/TeamCard/index.js
@@ -7,6 +7,15 @@ import TeamBackground from "../TeamBackground/";
 import TeamGuidelines from "../TeamGuidelines/";
 import TeamObjectives from "../TeamObjectives/";
 
+// function TeamCard ({team, index, openTeam}) {
+//    const { name, league, image, background, guidelines, objectives } = team;
+//    return (
+//      <CardWrap className={"team " + (team.open ? "open" : "")} key={index} onClick={() => openTeam(index)}>
+//
+//      </CardWrap>
+//    )
+// }
+
 class TeamCard extends React.Component {
   render() {
     const {
