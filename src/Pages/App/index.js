91546c2cac4f241a7312ce5f988dc2119ac035f1
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-09-29 21:41:56 -0400

    Create pages for the form and home, start building navbar

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -1,36 +1,74 @@
 import React from "react";
 import "../index.css";
+import styled from "styled-components";
 
-import { ContentWrap } from "../../Elements/ContentWrap/";
-import { Footer } from "../../Elements/Footer/";
+import logo from "../../data/images/CM_Logo.png";
 
-import Hero from "../../Components/Hero/";
-import Main from "../../Components/Main/";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "../Home/";
+import TeamFormPage from "../TeamFormPage/";
 
 function App() {
+  const media = { desktop: "@media(min-width: 1000px)" };
+
+  const NavWrap = styled.div`
+    margin: 0 auto;
+    max-width: 414px;
+
+    ${media.desktop} {
+      position: sticky;
+      max-width: 1200px;
+      margin: 0 auto;
+    }
+  `;
+
+  const Nav = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo {
+      width: 125px;
+      height: 40px;
+      padding: 16px 0 40px 0;
+      margin: 0 0 0 16px;
+    }
+
+    ${media.desktop} {
+      .logo {
+        width: 125px;
+        height: 40px;
+        padding: 40px 0;
+        margin: 0;
+      }
+    }
+  `;
+
   return (
-    <ContentWrap>
-      <Hero />
-      <Main />
-      <Footer>
-        <p>
-          If you like this site and its contents, please support us by buying us
-          a coffee. This way we can keep improving this page!
-        </p>
-        <a
-          className="bmc-button"
-          href="https://www.buymeacoffee.com/mannyandrebekah"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
-            alt="Buy us a coffee"
-          />
-          <span>Buy us a coffee?</span>
-        </a>
-      </Footer>
-    </ContentWrap>
+    <Router>
+      <NavWrap>
+        <Nav>
+          <Link to="/">
+            <img className="logo" src={logo} alt="logo" />
+          </Link>
+          <Link to="/teamform">Submit a Team</Link>
+          {/* <Link>Sign Up</Link> */}
+        </Nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/teamform">
+            <TeamFormPage />
+          </Route>
+          {/* <Route>
+            <SignUpPage />
+          </Route> */}
+        </Switch>
+      </NavWrap>
+    </Router>
   );
 }
 
---
+++ src/Pages/TeamFormPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TeamFormPage extends React.Component{
+    render() {
+        return(
+            <div>
+                <div>
+                    <p>next page, bitches</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TeamFormPage
